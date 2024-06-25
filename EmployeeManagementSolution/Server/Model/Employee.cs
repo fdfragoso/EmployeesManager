762382b8bac4d96c9100dca9b221fad8e13a9b99
Felipe Dacal Fragoso <fefragoso@gmail.com>
2024-06-25 22:11:21 +0200

    Set-up Server Web API

---
+++ EmployeeManagementSolution/Server/Model/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.Models
+{
+    [Table("Employees")]
+    public class Employee
+    {
+        [Key]
+        public int Id { get; private set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Photo { get; set; }
+
+        public Employee(string _name, int _age, string _photo)
+        {
+            this.Name = _name ?? throw new ArgumentNullException(nameof(Name));
+            this.Age = _age;
+            this.Photo = _photo;
+        }
+    }
+}
